e0757fef96bfe9d1085de474f8a1543d86cf9d68
Madhuvanthi Ramanujam <madhuraman87@gmail.com>
2023-07-14 14:26:30 -0700

    commit-5: Add unit test cases

--- test/client/routes/AllFiles/AllFilesView.test.js
+++ test/client/routes/AllFiles/AllFilesView.test.js
@@ -1,21 +1,76 @@
 import React from 'react'
 import { expect } from 'chai'
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
+import sinon from 'sinon'
 
 import AllFilesView, { Title } from 'client/routes/AllFiles/AllFilesView'
 
 describe('AllFilesView', function () {
-  it('renders the title', function () {
-    const wrapper = shallow(
-      <AllFilesView
-        username="Sally"
-        addFile={() => {}}
-        files={[]}
-      />,
-    )
+  let component
+  let clock
+  const addFilesMock = sinon.spy()
+  const mockFile = [
+    { id: 1, filename: 'file1.txt', src: 'file1.txt', description: 'File 1' },
+    { id: 2, filename: 'file2.txt', src: 'file2.txt', description: 'File 2' },
+  ]
+  // Mock props
+  const navigateMock = sinon.spy()
+  const locationMock = { search: '' }
 
-    const title = wrapper.find(Title)
+  beforeEach(() => {
+    // Render the component
+    component = mount(
+      <AllFilesView
+        addFile={addFilesMock}
+        username="Sally"
+        files={mockFile}
+        location={locationMock}
+        navigate={navigateMock}
+      />
+    )
+    clock = sinon.useFakeTimers()
+  })
+
+  afterEach(() => {
+    clock.restore()
+    component.unmount()
+  })
+
+  it('renders the title', function () {
+    const title = component.find(Title)
     expect(title).to.have.length(1)
     expect(title.text()).to.equal('Hi Sally 👋')
+  })
+
+  it('should render the SearchFiles component', () => {
+    const searchComponent = component.find('SearchFiles')
+    expect(searchComponent.exists()).to.be.true
+  })
+
+  it('should update state and call updateFilteredFileList when handleSearchQueryChange is called', () => {
+    const instance = component.instance()
+
+    // Spy on the necessary methods
+    const setQuerySpy = sinon.spy(instance, 'setState')
+    const updateFilteredFileListSpy = sinon.spy(instance, 'updateFilteredFileList')
+
+    // Simulate a change event on the search component
+    const newQuery = 'example'
+    instance.handleSearchQueryChange(newQuery)
+
+    expect(setQuerySpy.calledWith({ searchQuery: newQuery })).to.be.true
+    expect(navigateMock.calledOnce).to.be.true
+    expect(navigateMock.args[0][0]).to.deep.equal({ search: 'q=example' })
+    expect(updateFilteredFileListSpy.called).to.be.false
+
+    // Fast-forward the clock to trigger the debounce delay
+    clock.tick(300)
+
+    // Expectations after the debounce delay
+    expect(updateFilteredFileListSpy.calledOnce).to.be.true
+
+    // Restore the original methods
+    setQuerySpy.restore()
+    updateFilteredFileListSpy.restore()
   })
 })
