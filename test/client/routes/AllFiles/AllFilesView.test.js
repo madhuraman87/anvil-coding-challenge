1b5b6e52544d7ceb3aa92a92cc18cbf1ad78e994
Madhuvanthi Ramanujam <madhuraman87@gmail.com>
2023-07-14 14:42:07 -0700

    Task 2 file de dupe (#3)
    * commit-1: Linting changes
    
    * commit-2: Add business logic to handleFile method to handle duplication
    
    * commit-3: Add unit tests

--- test/client/routes/AllFiles/AllFilesView.test.js
+++ test/client/routes/AllFiles/AllFilesView.test.js
@@ -73,4 +73,33 @@ describe('AllFilesView', function () {
     setQuerySpy.restore()
     updateFilteredFileListSpy.restore()
   })
+  it('should call addFile with data when handleAddFile is called', () => {
+    const data = { file: { name: 'newFile.txt' } }
+    component.instance().handleAddFile(data)
+    expect(addFilesMock.calledWith(data)).to.be.true
+  })
+
+  it('should call addFile with data when handleAddFile is called with duplicate filename', () => {
+    const data = { file: { name: 'file1.txt' } }
+    component.instance().handleAddFile(data)
+    const expectedData = { ...data, file: { name: 'file1(1).txt' } }
+    expect(addFilesMock.calledWith(expectedData)).to.be.true
+  })
+
+  it('should add a file with a unique filename when a duplicate filename is provided', () => {
+    const duplicateFilename = 'file1.txt'
+    const data = { file: { name: duplicateFilename } }
+
+    const addFileStub = sinon.stub().returnsArg(0)
+    component.setProps({ addFile: addFileStub })
+
+    component.instance().handleAddFile(data)
+
+    // Check if addFileStub was called with a unique filename
+    expect(addFileStub.calledOnce).to.be.true
+    const calledWithFileData = addFileStub.args[0][0]
+    expect(calledWithFileData.file.name).to.not.equal(duplicateFilename)
+    expect(calledWithFileData.file.name).to.match(/file1\(.*\).txt/)
+  })
+
 })
